c6fca3270725be73f607450371614267c9780fc2
Kamad Saxena <sansax1112@gmail.com>
2020-12-13 17:59:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ image_source2 = "images\\dice"+randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src", image_source2);
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩"+player1+"Wins!";
+  document.querySelector("h1").innerHTML = player1+" Wins! 🚩";
 }
 else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = player2 + "Wins! 🚩";
+  document.querySelector("h1").innerHTML = player2 + " Wins! 🚩";
 }
 else {
   document.querySelector("h1").innerHTML = "Draw!";
