5f907b952e7f51a7a88a4bcd5f7d822299b017b9
Kamad Saxena <sansax1112@gmail.com>
2020-12-13 13:27:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ var player2 = prompt("Enter name for player2: ");
 document.getElementsByTagName("p")[1].innerHTML = player2;
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-image_source1 = "images\\dice"+randomNumber1+".png"
-document.querySelectorAll("img")[0].setAttribute("src", image_source1)
+image_source1 = "images\\dice"+randomNumber1+".png";
+document.querySelectorAll("img")[0].setAttribute("src", image_source1);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-image_source2 = "images\\dice"+randomNumber2+".png"
-document.querySelectorAll("img")[1].setAttribute("src", image_source2)
+image_source2 = "images\\dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src", image_source2);
 
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩"+player1+"Wins!";
